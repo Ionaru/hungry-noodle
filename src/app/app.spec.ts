2ec624da7ddfcbbf99211e4712f353c0ad68badf
Jeroen Akkerman <Jeroen_akkerman1@hotmail.com>
2025-07-31 13:23:53 +0200

    Initial commit

---
+++ src/app/app.spec.ts
@@ -0,0 +1,32 @@
+import { describe, test, expect, beforeEach } from "vitest";
+import { provideZonelessChangeDetection } from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { App } from "./app";
+
+describe("App", () => {
+  let component: App;
+  let fixture: ComponentFixture<App>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [App],
+      providers: [provideZonelessChangeDetection()],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(App);
+    component = fixture.componentInstance;
+
+    fixture.detectChanges();
+  });
+
+  test("should create the app", () => {
+    expect(component).toBeTruthy();
+  });
+
+  test("should render title", () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector("h1")?.textContent).toContain(
+      "Welcome to Tauri + Angular!"
+    );
+  });
+});
